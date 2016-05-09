094736f74441d7daa39d1d33a009b89407a8a99b
Renan <renan.061@gmail.com>
2016-05-09 15:19:51 -0300

    Naming service setup and howto.txt to log what was done (configs)

--- client.py
+++ client.py
@@ -2,7 +2,7 @@
 
 import sys
 from omniORB import CORBA, PortableServer
-import INF1822, INF1822__POA
+import CosNaming, INF1822
 
 # 
 # Client
@@ -13,15 +13,30 @@ print "Client started..."
 # ORB setup
 orb = CORBA.ORB_init(sys.argv, CORBA.ORB_ID)
 
-# Object
-ior = sys.argv[1]
-obj = orb.string_to_object(ior)
-device = obj._narrow(INF1822.Device)
+# Obtain a reference to the root naming context
+obj = orb.resolve_initial_references("NameService")
+rootContext = obj._narrow(CosNaming.NamingContext)
+if rootContext is None:
+    print "Failed to narrow the root naming context"
+    sys.exit(1)
 
+# Resolve the name "server.inf1822/INF1822Device.Object"
+name = [CosNaming.NameComponent("server", "inf1822"),
+        CosNaming.NameComponent("INF1822Device", "Object")]
+try:
+    obj = rootContext.resolve(name)
+except CosNaming.NamingContext.NotFound, ex:
+    print "Name not found"
+    sys.exit(1)
+
+# Narrow the object to an INF1822::Device
+device = obj._narrow(INF1822.Device)
 if device is None:
-	print "Object reference is not an Example::Echo"
-else:
-	print device.id
-	print device.type
+    print "Object reference is not an INF1822::Device"
+    sys.exit(1)
+
+# Doing something
+print device.id
+print device.type
 
 print "Client finished..."
--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 
 import sys
 from omniORB import CORBA, PortableServer
-import INF1822, INF1822__POA
+import CosNaming, INF1822, INF1822__POA
 
 # 
 # Implementations
@@ -22,18 +22,44 @@ print "Server started..."
 
 # ORB setup
 orb = CORBA.ORB_init(sys.argv, CORBA.ORB_ID)
+poa = orb.resolve_initial_references("RootPOA")
 
 # Object implementation
 deviceServant = DeviceServant(1132, "master")
-device = deviceServant._this() # How does this work? / Naming wrong
+device = deviceServant._this() # How does this work?
 
-# FIXME: Move this somewhere else
-print orb.object_to_string(device) # IOR
+# Naming service
+obj = orb.resolve_initial_references("NameService");
+rootContext = obj._narrow(CosNaming.NamingContext)
 
-# Activating the POA
-poa = orb.resolve_initial_references("RootPOA")
+if rootContext is None:
+    print "Failed to narrow the root naming context"
+    sys.exit(1)
+
+# Bind a context named "server.inf1822" to the root context
+name = [CosNaming.NameComponent("server", "inf1822")]
+try:
+    testContext = rootContext.bind_new_context(name)
+    print "New test context bound"
+except CosNaming.NamingContext.AlreadyBound, ex:
+    print "Test context already exists"
+    obj = rootContext.resolve(name)
+    testContext = obj._narrow(CosNaming.NamingContext)
+    if testContext is None:
+        print "server.inf1822 exists but is not a NamingContext"
+        sys.exit(1)
+
+# Bind the Device object to the test context
+name = [CosNaming.NameComponent("INF1822Device", "Object")]
+try:
+    testContext.bind(name, device)
+    print "New INF1822Device object bound"
+except CosNaming.NamingContext.AlreadyBound:
+    testContext.rebind(name, device)
+    print "INF1822Device binding already existed -- rebound"
+
+# Activate the POA and wait
 poa._get_the_POAManager().activate()
-
 orb.run()
 
 print "Server finished..."
