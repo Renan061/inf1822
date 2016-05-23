0ddd3ba95325bb137eb4e0fe3669403647a1ed80
Renan <renan.061@gmail.com>
2016-05-23 17:48:50 -0300

    Mutex in the naming service

--- naming-service.py
+++ naming-service.py
@@ -2,6 +2,7 @@
 
 import aux
 import INF1822, INF1822__POA
+import threading
 
 # ==================================================
 #
@@ -12,39 +13,64 @@ import INF1822, INF1822__POA
 class CatalogueImpl(INF1822__POA.Catalogue):
 	# Default constructor
 	def __init__(self):
-		self._dictionary = {}
+		self._typeDictionary = {}
+		self._lock = threading.Lock()
 
 	def register(self, ior, name, type):
-		# Check if name is string
-		# Check if ior is string
-		if self._dictionary.has_key(name):
-			# FIXME: Return more information
-			# return False
-			return True
-		self._dictionary[name] = ior
-		# TODO: Do something with type
+		if not isinstance(ior, basestring) or not isinstance(name, basestring):
+			return False
+
+		self._lock.acquire()
+		if not self._typeDictionary.has_key(type):
+			self._typeDictionary[type] = {}
+		self._typeDictionary[type][name] = ior
+		self._lock.release()
+
 		print("Registered <" + name + ">")
 		return True
 
 	def deregister(self, ior, name):
-		# TODO
-		print("CatalogueImpl deregister")
-		return False
+		value = False
+		self._lock.acquire()
+		for key, nameDictionary in self._typeDictionary.items():
+			if nameDictionary.get(name) is ior:
+				del nameDictionary[name]
+				if len(nameDictionary) is 0:
+					del self._typeDictionary[key]
+				value = True
+				break
+		self._lock.release()
+		return value
 
 	def getByName(self, name):
-		if self._dictionary.has_key(name):
-			return self._dictionary[name]
-		return "error"
+		value = None
+		self._lock.acquire()
+		for key, nameDictionary in self._typeDictionary.items():
+			if nameDictionary.has_key(name):
+				value = nameDictionary[name]
+				break
+		self._lock.release()
+		return value
 
 	def getByType(self, type):
-		# TODO
-		print("CatalogueImpl getByType")
-		return ["not", "-", "implemented"]
+		iorList = []
+		self._lock.acquire()
+		for name, ior in self._typeDictionary[type].items():
+			iorList.append(ior)
+		self._lock.release()
+		return iorList
 
 	def getMasterForType(self, type):
-		# TODO
-		print("CatalogueImpl getMasterForType")
-		return "not-implemented"
+		self._lock.acquire()
+		nameDictionary = self._typeDictionary.get(type)
+		
+		if nameDictionary is None:
+			self._lock.release()
+			return None
+
+		value = nameDictionary.get("master")
+		self._lock.release()
+		return value # TODO
 
 # ==================================================
 #
