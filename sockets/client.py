0a5765742b5df0f4e5312feb4a4abc3e3d0c6a31
Renan <renan.061@gmail.com>
2016-06-13 16:32:28 -0300

    Basic echoing example

---
+++ sockets/client.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import socket
+
+serverAddress = ('localhost', 1984)
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect(serverAddress)
+clientSocket.send("Hi")
+clientSocket.close()
